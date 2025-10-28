3aebbdf31b578a28ceefc419d4f64dfdfc16d35d
quankane <quanducbui2017@gmail.com>
2025-10-28 17:04:15 +0700

    Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { lazy, Suspense, useEffect } from "react";
+import "./App.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import { useRoutes } from "react-router-dom";
+
+const Home = lazy(() => import("@/pages/Home"));
 
 function App() {
-  const [count, setCount] = useState(0)
+    useEffect(() => {
+        Aos.init({
+            duration: 1000,
+            once: true,
+        });
+    }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const routes = useRoutes([
+        {
+            path: "/",
+            element: <Home />,
+        },
+    ]);
+
+    return <Suspense fallback={<div>Loading...</div>}>{routes}</Suspense>;
 }
 
-export default App
+export default App;
